2d88ec0415640fc89fc8fa9ebb1d4e2f97a422e6
Pankaj Rane <pankaj.rane@whitehatjr.com>
2023-04-21 15:41:55 +0530

    first commit

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,125 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="icon" href="{{ asset('images/favicon.ico') }}" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.6.1/font/bootstrap-icons.css">
+    <link rel="stylesheet" type="text/css" href="{{ asset('css/app.css') }}" />
+
+    <title>Extreme Adventure</title>
+
+</head>
+
+<body>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous">
+        < script src = "https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"
+        integrity = "sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3"
+        crossorigin = "anonymous" >
+            <
+            /> <
+        script src = "https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js"
+        integrity = "sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V"
+        crossorigin = "anonymous" >
+    </script>
+    </script>
+    <main class="main-container">
+        <div class="container container-fluid">
+            <div>Karma Yoga, you have 3 activities live on Switch.</div>
+            <div>You've had 6,524 people look at your activities this month.</div>
+            <div>You managed to turn that into 52 bookings.</div>
+            <div class="row">
+                <div class="col card bg-opacity-10 bg-info">
+                    <div class="card-body flex">
+                        <div class="row">
+                            <div class="col-10">
+                                <div class="dropdown">
+                                    <button class="btn dropdown-toggle bg-none border-0" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                      Dropdown button
+                                    </button>
+                                    <ul class="dropdown-menu">
+                                      <li><a class="dropdown-item" href="#">Action</a></li>
+                                      <li><a class="dropdown-item" href="#">Another action</a></li>
+                                      <li><a class="dropdown-item" href="#">Something else here</a></li>
+                                    </ul>
+                                  </div>
+                            </div>
+                            <div class="col text-end d-inline">
+                                <i class="bi bi-arrow-right-short align-middle fs-4"></i>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col">
+                                <i class="bi bi-bullseye fs-4"></i><span class="fs-2 p-2">6,800</span>AED
+                                <p class="fw-bolder ms-4 text-opacity-50 text-secondary text-uppercase">Total this month</p>
+                            </div>
+                            <div class="col">
+                                <i class="bi bi-bullseye fs-4"></i><span class="fs-2 p-2">4,100</span>AED
+                                <p class="fw-bolder ms-4 text-opacity-50 text-secondary text-uppercase">From switch+ members</p>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col">
+                                <i class="bi bi-bullseye fs-4"></i><span class="fs-2 p-2">670</span>
+                                <p class="fw-bolder ms-4 text-opacity-50 text-secondary text-uppercase">bookings this month</p>
+                            </div>
+                            <div class="col">
+                                <i class="bi bi-eye icon-circle border border-dark fs-5"></i><span class="fs-2 p-2">2,390</span>
+                                <p class="fw-bolder ms-4 text-opacity-50 text-secondary text-uppercase">views this month</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col card bg-opacity-10 bg-info ms-3">
+                    <div class="card-body flex">
+                        <div class="row">
+                            <div class="col-10">
+                                <div class="dropdown">
+                                    <button class="btn dropdown-toggle btn-outline-dark" href="#" role="button"
+                                        data-bs-toggle="dropdown" aria-expanded="false">
+                                        Dropdown link
+                                    </button>
+                                    <ul class="dropdown-menu">
+                                        <li><a class="dropdown-item" href="#">Action</a></li>
+                                        <li><a class="dropdown-item" href="#">Another action</a></li>
+                                        <li><a class="dropdown-item" href="#">Something else here</a></li>
+                                    </ul>
+                                </div>
+                            </div>
+                            <div class="col text-end d-inline">
+                                <i class="bi bi-arrow-right-short align-middle fs-4"></i>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col">
+                                <i class="bi bi-bullseye fs-4"></i><span class="fs-2 p-2">6,800</span>AED
+                                <p class="fw-bolder ms-4 text-opacity-50 text-secondary">TOTAL THIS MONTH</p>
+                            </div>
+                            <div class="col">
+                                <i class="bi bi-bullseye fs-4"></i><span class="fs-2 p-2">670</span>AED
+                                <p class="fw-bolder ms-4 text-opacity-50 text-secondary">TOTAL THIS MONTH</p>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="col">
+                                <i class="bi bi-bullseye fs-4"></i><span class="fs-2 p-2">6,800</span>AED
+                                <div>TOTAL THIS MONTH</div>
+                            </div>
+                            <div class="col">
+                                <i class="bi bi-eye fs-4"></i><span class="fs-2 p-2">6,800</span>AED
+                                <div>TOTAL THIS MONTH</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
+</body>
+
+</html>
